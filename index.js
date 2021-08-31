286044c77c5fac2f1471b6d0a77ab6534c0c937f
nizamseu <nizamcse.seu@gmail.com>
2021-08-31 12:49:57 +0600

    single data load complete

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ console.log(meal);
         const div = document.createElement('div');
         // div.classList.add('col');
         div.innerHTML = `
-        <div class="col">
+        <div onClick="loadSingleMeal(${item.idMeal})" class="col">
               <div class="card">
                 <img src="${item.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -32,5 +32,32 @@ console.log(meal);
         `;
         cardItem.appendChild(div)
     })
-    console.log(meal);
+  
+}
+
+
+loadSingleMeal = (id)=>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=> displaySingleMeal(data?.meals[0]))
+}
+
+
+displaySingleMeal=meal=>{
+    const singleMeal =document.getElementById('singleMeal');
+    
+        const div = document.createElement('div');
+        div.classList.add('card')
+
+        div.innerHTML = `
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body my-3">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0,100)}</p>
+                <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+            </div>
+        `;
+        singleMeal.appendChild(div)
+console.log(meal);
 }
