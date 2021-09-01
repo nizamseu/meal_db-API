c0eb5d6f5b78405f8d97c1e30d665af632d70cb0
nizamseu <nizamcse.seu@gmail.com>
2021-09-01 20:00:34 +0600

    added sppiner and some error handling is done

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ searchMeal = ()=>{
     const inputField =document.getElementById('inputField');
     const inputValue = inputField.value;
     const emptyInput =document.getElementById('emptyInput');
-
+   
     if(inputValue == ''){
-       
-       emptyInput.style.display='block'
+       emptyInput.style.display='block';
+       const cardItem =document.getElementById('cardItem');
+       cardItem.textContent ='';
     }else{
+        sppiner('visible')
         emptyInput.style.display='none'
         const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
     
@@ -14,16 +16,12 @@ searchMeal = ()=>{
         .then(res=>res.json())
         .then(data=>displayMeal(data.meals))
     }
-
-    
-
     inputField.value=''
-  
 }
 
 displayMeal= (meal)=>{
 
-    console.log(meal);
+    // console.log(meal);
     // clean serach result and single prvious data 
     const singleMeal =document.getElementById('singleMeal');
     const cardItem =document.getElementById('cardItem');
@@ -35,15 +33,15 @@ displayMeal= (meal)=>{
 const error =document.getElementById('error');
     if(meal === null){
         error.style.display='block'
-        console.log(error);
-        console.log("painai");
+        // console.log(error);
+        // console.log("painai");
     }else {
         error.style.display='none'
         meal.map(item=>{
             const div = document.createElement('div');
             // div.classList.add('col');
             div.innerHTML = `
-            <div onClick="loadSingleMeal(${item.idMeal})" class="col">
+            <div id="img" onClick="loadSingleMeal(${item.idMeal})" class="col">
                   <div class="card">
                     <img src="${item.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
@@ -54,11 +52,12 @@ const error =document.getElementById('error');
             </div>
             `;
             cardItem.appendChild(div)
+            
         })
     }
-
-
-    
+    sppiner('hidden')
+   
+   
   
 }
 
@@ -82,13 +81,19 @@ displaySingleMeal=meal=>{
             <div class="card-body my-3">
                 <h5 class="card-title">${meal.strMeal}</h5>
                 <p class="card-text">${meal.strInstructions.slice(0,100)}</p>
-                <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+                <a href="${meal.strYoutube}" class="btn btn-primary">Watch Video </a>
             </div>
         `;
         singleMeal.appendChild(div)
 
 }
 
+// sppiner function 
+    sppiner = (property)=>{
+    const sppiner = document.getElementById('sppiner');
+    console.log(sppiner);
+    sppiner.style.visibility= property
+    }
 
-const bondCode=` I am Fake James bond . My new code is: 00${7+1+2}`
-console.log(bondCode);
+
+    // sppiner('hidden')
